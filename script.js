aeb3bcc75cac0b32c2f5c4c8c65a3fb2cc43217a
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-07-12 11:40:15 +0530

    data types

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+// ************************    BASISCS      ************************
+/*
 let js = "amaizing";
 if (js === "amaizing") alert("JavaScript is FUN!!!");
 
 console.log(48 + 10 + 18);
+*/
 
+// ********************      VARIABLES & VALUES     ******************
+/*
  let firstName = "faiz";
  let profession="Programmer"
  console.log(firstName , profession);
@@ -10,3 +15,26 @@ console.log(48 + 10 + 18);
  let job1= 'Programmer';
  let job2='Senior Developer';
  console.log(` job1 is ${job1} and job2 is ${job2}`);
+*/
+
+// ************************   DATA TYPES  ************************
+
+let javaSriptIsFun = true;
+console.log(javaSriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof "faiz");
+// console.log(typeof 23);
+// console.log(typeof " ");
+
+javaSriptIsFun = "YESS!!";
+console.log(typeof javaSriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1998;
+console.log(typeof year);
+
+console.log(typeof null);
