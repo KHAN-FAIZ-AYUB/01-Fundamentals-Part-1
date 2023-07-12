27c818b2cebec2eb7a08ce94d0eeceb3bcb727a5
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-07-12 11:12:40 +0530

    Variables & Values

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let js = "amaizing";
 if (js === "amaizing") alert("JavaScript is FUN!!!");
 
-48 + 10 + 18;
 console.log(48 + 10 + 18);
+
+ let firstName = "faiz";
+ let profession="Programmer"
+ console.log(firstName , profession);
+
+ let job1= 'Programmer';
+ let job2='Senior Developer';
+ console.log(` job1 is ${job1} and job2 is ${job2}`);
