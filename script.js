662c63c15fb89163b7f06e2f8c5dbaf7810847ce
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-07-18 17:34:31 +0530

    ternary operator

--- script.js
+++ script.js
@@ -358,4 +358,25 @@ if (day === "monday") {
 }
 */
 
-// *********************  STATEMENT  & EXPRESIONS *********************
+// *********************  TERNARY OPERATOR *********************
+/*
+const age = 23;
+// age >= 18
+//   ? console.log("I like to drink wine")
+//   : console.log("i like to drink water");
+
+const drink = age >= 18 ? "wine" : "water";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine";
+} else {
+  drink2 = "water";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+*/
+
+// *********************  CHALLENGE #04  *********************
